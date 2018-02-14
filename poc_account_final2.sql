4045f9cb952df951aaa2f5ce585a404a7251e212
Friedhold Matz <fmatz.com@gmail.com>
2018-02-14 18:08:45 +0100

    Update poc_account_final2.sql

--- poc_account_final2.sql
+++ poc_account_final2.sql
@@ -555,15 +555,15 @@ BEGIN
    	ELSE
    	   Set_Item_Property(l_blk||'.'||'LABEL_'||l_bit , VISUAL_ATTRIBUTE, 'VA_TXT_LABEL_OK');
    	   prc_enable_item(l_blk||'.'||'UNDER_'||l_bit);
-   		 	   Set_Item_Property(l_blk||'.'||'UNDER_'||l_bit , VISUAL_ATTRIBUTE, 'VA_UL_FULL'); 
-   		 	   IF l_vres<>'OK' THEN
-   		 	      prc_enable_item(l_blk||'.'||'MSG_'||l_bit);
-   		 	      Set_Item_Property(l_blk||'.'||'MSG_'||l_bit , VISUAL_ATTRIBUTE, fnc_sign_msg(l_vres));
-   		 	      COPY(l_vres, l_blk||'.'||'MSG_'||l_bit);
-   		 	   END IF;
-	   	  END IF;
-   	 ELSE
-   	    prc_enable_item(l_blk||'.'||'LABEL_'||l_bit); 	
+   	   Set_Item_Property(l_blk||'.'||'UNDER_'||l_bit , VISUAL_ATTRIBUTE, 'VA_UL_FULL'); 
+   	   IF l_vres<>'OK' THEN
+   	      prc_enable_item(l_blk||'.'||'MSG_'||l_bit);
+   	      Set_Item_Property(l_blk||'.'||'MSG_'||l_bit , VISUAL_ATTRIBUTE, fnc_sign_msg(l_vres));
+   	      COPY(l_vres, l_blk||'.'||'MSG_'||l_bit);
+   	   END IF;
+	END IF;
+   	ELSE
+   	   prc_enable_item(l_blk||'.'||'LABEL_'||l_bit); 	
    	    Set_Item_Property(l_blk||'.'||'LABEL_'||l_bit , VISUAL_ATTRIBUTE, 'VA_TXT_LABEL_ERROR');
   	    -- underline to error full  --
    	    prc_enable_item(l_blk||'.'||'UNDER_'||l_bit);
@@ -585,12 +585,12 @@ BEGIN
    FOR i IN 1.. pkg_Item.item_ix.count LOOP
        l_value:= NAME_IN(pkg_Item.item_ix(i).name);
        IF (l_value IS NULL AND pkg_Item.item_ix(i).notnull='YES') OR 
-  	   	  -- item label text ? --
-  	   	  (l_value=pkg_Item.item_ix(i).text AND pkg_Item.item_ix(i).notnull='YES') OR
-  	   	  -- item error message ? --
-  	   	  substr(NAME_IN(pkg_Item.item_name(pkg_Item.item_ix(i).name).block||'.'||
+  	  -- item label text ? --
+  	  (l_value=pkg_Item.item_ix(i).text AND pkg_Item.item_ix(i).notnull='YES') OR
+  	  -- item error message ? --
+  	  substr(NAME_IN(pkg_Item.item_name(pkg_Item.item_ix(i).name).block||'.'||
   	   	                 'MSG_'||pkg_Item.item_ix(i).name),1,3)='$$$' THEN 	   	  
-  	   	  RETURN('Item: '|| pkg_Item.item_ix(i).name);
+  	  RETURN('Item: '|| pkg_Item.item_ix(i).name);
        END IF; 		 
    END LOOP;
    
@@ -610,36 +610,37 @@ BEGIN
       Execute_Trigger('WHEN-BUTTON-PRESSED');  
       sleep(100); 
    ELSE 
-	 	   -- setter & getter item values --
-			 Execute_Trigger('WHEN-NEW-ITEM-INSTANCE');  
-			 Copy(p_value, p_block||'.'||p_item);
-			 Execute_Trigger('WHEN-VALIDATE-ITEM');
-			 sleep(150);
-			 -- check expected/real result --
-			 l_res:='OK';
-			 IF (substr(NAME_IN(pkg_Item.item_name(p_item).block||'.'||'MSG_'||p_item) ,1,3)='$$$') THEN
-			 	  l_res:='NOK';
-			 END IF;
-			 IF (l_res='NOK' AND p_result='OK') OR
-		  	  ( (Name_In('MSG_'||p_item) IS NULL OR l_res='OK') AND p_result='NOK' )
-		  	  THEN
-		  	    prc_info('$$$ Error in automatic test sequence :: '||chr(10)||
-		  	             'Item: '||p_item||chr(10)||
-		  	             'Value:'||p_value||chr(10)||
-		  	             'Result expected: '||p_result||chr(10)||
-		  	             'Result real: '||l_res||chr(10)||' $$$');
-		  	  RETURN;
-			 END IF;			 
-			 -- compare values : setter=getter ? --
-			 IF Name_In(pkg_Item.item_name(p_item).block||'.'||p_item)<>p_value THEN
-			 	  prc_info('$$$ Error in automatic test sequence :: '||chr(10)||
-			 	           'Item: '||p_item||chr(10)||
-			 	           'Value expected: '||p_value||chr(10)||
-			 	           'Value real: '||Name_In(pkg_Item.item_name(p_item).block||'.'||p_item)||chr(10)||' $$$');
-			 END IF; 
-	 END IF; 
-	 EXCEPTION WHEN OTHERS THEN
-  	  prc_info('$$$ Exception in pkg_Item.prc_chk_Item: '||sqlerrm); 	 
+      -- setter & getter item values --
+      Execute_Trigger('WHEN-NEW-ITEM-INSTANCE');  
+      Copy(p_value, p_block||'.'||p_item);
+      Execute_Trigger('WHEN-VALIDATE-ITEM');
+      sleep(150);
+      -- check expected/real result --
+      l_res:='OK';
+      IF (substr(NAME_IN(pkg_Item.item_name(p_item).block||'.'||'MSG_'||p_item) ,1,3)='$$$') THEN
+         l_res:='NOK';
+      END IF;
+      IF (l_res='NOK' AND p_result='OK') OR
+	 ( (Name_In('MSG_'||p_item) IS NULL OR l_res='OK') AND p_result='NOK' )
+	 THEN
+	 prc_info('$$$ Error in automatic test sequence :: '||chr(10)||
+		  'Item: '||p_item||chr(10)||
+		  'Value:'||p_value||chr(10)||
+		  'Result expected: '||p_result||chr(10)||
+		  'Result real: '||l_res||chr(10)||' $$$');
+	 RETURN;
+      END IF;			 
+      -- compare values : setter=getter ? --
+      IF Name_In(pkg_Item.item_name(p_item).block||'.'||p_item)<>p_value THEN
+         prc_info('$$$ Error in automatic test sequence :: '||chr(10)||
+	          'Item: '||p_item||chr(10)||
+		  'Value expected: '||p_value||chr(10)||
+		  'Value real: '||Name_In(pkg_Item.item_name(p_item).block||'.'||p_item)||chr(10)||' $$$');
+      END IF; 
+   END IF; 
+		       
+   EXCEPTION WHEN OTHERS THEN
+      prc_info('$$$ Exception in pkg_Item.prc_chk_Item: '||sqlerrm); 	 
 END prc_chk_item;
 -----------------------------------------------------------------------------------
 -- !!! Used prc_Set_Items - defined from USER external procedure !!! --
@@ -650,52 +651,52 @@ PROCEDURE prc_rec (p_ix PLS_INTEGER, p_block VARCHAR2, p_name VARCHAR2, p_label 
     l_text  VARCHAR2(64);
 BEGIN  	
    IF p_notnull='YES' THEN
-  	  l_label:= p_label||' *';
-  	  l_text := p_text||' *';
+      l_label:= p_label||' *';
+      l_text := p_text||' *';
    ELSE
-  	  l_label:= p_label;
-  	  l_text := p_text;
+      l_label:= p_label;
+      l_text := p_text;
    END IF;
-	 -- 1. set name sorted tab --
-	 pkg_Item.item_name(p_name).id		  := p_ix;
-	 pkg_Item.item_name(p_name).block   := p_block;
-	 pkg_Item.item_name(p_name).name	  := p_name;
-	 pkg_Item.item_name(p_name).label   := l_label;
-	 pkg_Item.item_name(p_name).text    := l_text;
-	 pkg_Item.item_name(p_name).msg     := p_messg;  
-	 pkg_Item.item_name(p_name).notnull := p_notnull;   
-	 pkg_Item.item_name(p_name).type	 	:= p_type;
+   -- 1. set name sorted tab --
+   pkg_Item.item_name(p_name).id      := p_ix;
+   pkg_Item.item_name(p_name).block   := p_block;
+   pkg_Item.item_name(p_name).name    := p_name;
+   pkg_Item.item_name(p_name).label   := l_label;
+   pkg_Item.item_name(p_name).text    := l_text;
+   pkg_Item.item_name(p_name).msg     := p_messg;  
+   pkg_Item.item_name(p_name).notnull := p_notnull;   
+   pkg_Item.item_name(p_name).type    := p_type;
 		    	  
-	 -- 2. set ix sorted tab --
- 	 pkg_Item.item_ix(p_ix).id		      := p_ix;
-	 pkg_Item.item_ix(p_ix).block       := p_block;
-	 pkg_Item.item_ix(p_ix).name	      := p_name;
- 	 pkg_Item.item_ix(p_ix).label       := l_label;
-	 pkg_Item.item_ix(p_ix).text        := l_text;
-	 pkg_Item.item_ix(p_ix).msg         := p_messg;
-	 pkg_Item.item_ix(p_ix).notnull     := p_notnull;
-	 pkg_Item.item_ix(p_ix).type	      := p_type;	
+   -- 2. set ix sorted tab --
+   pkg_Item.item_ix(p_ix).id	      := p_ix;
+   pkg_Item.item_ix(p_ix).block       := p_block;
+   pkg_Item.item_ix(p_ix).name	      := p_name;
+   pkg_Item.item_ix(p_ix).label       := l_label;
+   pkg_Item.item_ix(p_ix).text        := l_text;
+   pkg_Item.item_ix(p_ix).msg         := p_messg;
+   pkg_Item.item_ix(p_ix).notnull     := p_notnull;
+   pkg_Item.item_ix(p_ix).type	      := p_type;	
 		 		        
-	EXCEPTION WHEN OTHERS THEN
-  	 prc_info('$$$ Exception in pkg_Item.prc_init(rec): '||sqlerrm); 
+   EXCEPTION WHEN OTHERS THEN
+      prc_info('$$$ Exception in pkg_Item.prc_init(rec): '||sqlerrm); 
 END prc_rec;
 
 PROCEDURE prc_init_Items IS 
-  l_name VARCHAR2(32);
- BEGIN 	
- 	 -- USER DEFINED - external !!! --
- 	 prc_Set_Items;
+   l_name VARCHAR2(32);
+BEGIN 	
+   -- USER DEFINED - external !!! --
+   prc_Set_Items;
  	 
    FOR i IN 1.. pkg_Item.item_ix.count LOOP
-  	   l_name:= pkg_Item.item_ix(i).name; 	       
-  	   COPY(pkg_Item.item_ix(i).text,  l_name);  
-  	   COPY(pkg_Item.item_ix(i).label, 'LABEL_'||l_name);   
-  	   -- specials --
-  	   IF pkg_Item.item_ix(i).type='SECURE' THEN
-  	  	  -- shows text --
-  	      Set_item_Property(pkg_Item.item_ix(i).block||'.'||l_name, ECHO, PROPERTY_TRUE); 	     
-  	   END IF; 	  
-  	   Set_item_Property(pkg_Item.item_ix(i).block||'.UNDER_'||l_name, VISUAL_ATTRIBUTE, 'VA_UL_EMPTY');		 		  		 
+       l_name:= pkg_Item.item_ix(i).name; 	       
+       COPY(pkg_Item.item_ix(i).text,  l_name);  
+       COPY(pkg_Item.item_ix(i).label, 'LABEL_'||l_name);   
+       -- specials --
+       IF pkg_Item.item_ix(i).type='SECURE' THEN
+  	  -- shows text --
+  	  Set_item_Property(pkg_Item.item_ix(i).block||'.'||l_name, ECHO, PROPERTY_TRUE); 	     
+       END IF; 	  
+       Set_item_Property(pkg_Item.item_ix(i).block||'.UNDER_'||l_name, VISUAL_ATTRIBUTE, 'VA_UL_EMPTY');		 		  		 
    END LOOP;
   
  EXCEPTION WHEN OTHERS THEN
@@ -709,7 +710,7 @@ PROCEDURE prc_Set_Items IS
    -- Friedhold Matz - 2018-FEB --
    -- item value definitions --
 BEGIN
-	 -- place your item definitions here . ! --
+   -- place your item definitions here . ! --
    pkg_Item.prc_rec(1 , 'BLK_ACCOUNT', 'USERNAME', 		     'User name', 					 	
 		        'Enter your user name', 	             'Must begin with .. followed .. #_$');
 		         
